816fb567783307fa1dff535c838a2e57a25e6646
Yao(Victor) Li <victorliyao@gmail.com>
2023-05-21 22:05:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,74 +6,71 @@ app.use(cors());
 
 
 
-const API_KEY = "7OlEKeJ4zbqZGYvVYOrD4rMlLgWdCU5j";
-
-app.get('/weather/basec', (req, res) => {
+app.get('/con/:base/:target/:amo', (req, res) => {
   console.log("welcome to the root!");
-    //////////////////////////////
-  // Get the Strings of inputs
+
+  var base = req.params.base;
+  var target = req.params.target;
+  var amo = req.params.amo;
+
+  var myHeaders = new Headers();
+  myHeaders.append("apikey", "7OlEKeJ4zbqZGYvVYOrD4rMlLgWdCU5j");
   
-  var url = `https://api.apilayer.com/currency_data/convert?base=USD&symbols=EUR,GBP,JPY&amount=5&date=2018-01-01' \
-  --header 'apikey:${API_KEY}`;  
-	request(url, (error, response, body)=>{
-		
-		// Printing the error if occurred
-		if(error) console.log('Error occured:',error)
-	   
-		// Printing status code
-		console.log(response.statusCode);
-		
-        
-		// Printing TEMP
-        const data = JSON.parse(body);
-        console.log('API Response:', data);
-        
-        
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
+  var resultBody;
+  
+  fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${target}&from=${base}&amount=${amo}`, requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      resultBody = result;
+      res.send({"result": resultBody.result});
+    })
+    .catch(error => console.log('error', error));
+  
 	});
-});
 
-
-// Extra credit: 5 Day
-app.get('/5day/:lat/:lon', (req, res) => {
-    console.log("5-day forecast(In Fahrenheit)");
-    var lat = req.params.lat;
-    var lon = req.params.lon;
+  app.get('/7day/:base/:target', (req, res) => {
+    console.log("welcome to the root!");
     
-    var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`;
-    
-      request(url, (error, response, body)=>{
-          
-          // Printing the error if occurred
-          if(error) console.log(error)
-         
-          // Printing status code
-          console.log('StatusCode:',response.statusCode);
-          
-          // Calculate Average TEMP for each day
-          const data = JSON.parse(body);
-          const week = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-          const forecast = [];
-          let todaysDate = new Date().getDay()
-          
-          for (let i=0;i<5;i++){
-            let tempSum = 0
-            let count = 0
-            for (let dataPoint of data.list){
-              const date = new Date(dataPoint.dt*1000)
-              if (date.getDay() == todaysDate){
-                count++;
-                tempSum += dataPoint.main.temp;
-              }
-            }
-            const day={"dayName": week[todaysDate], "temp": Math.round(tempSum / count) }
-            forecast.push(day)
-            todaysDate = (todaysDate + 1) % 7
-          }
-          res.send({forecast})
+    //Parameters
+    //base: default currency,  target: currency that convert to
+    var base = req.params.base;
+    var target = req.params.target;
 
-          
-      });
-  });
+    //Get Today's date
+    let date = new Date();
+    let today = date.toISOString().split("T")[0];
+
+    //Get the date of 7 days ago
+    date.setDate(date.getDate() - 7);
+    let day7 = date.toISOString().split("T")[0];
+
+  
+    var myHeaders = new Headers();
+    myHeaders.append("apikey", "7OlEKeJ4zbqZGYvVYOrD4rMlLgWdCU5j");
+    
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+      headers: myHeaders
+    };
+    var resultBody;
+    
+    fetch(`https://api.apilayer.com/exchangerates_data/timeseries?start_date=${day7}&end_date=${today}&symbols=${target}&base=${base}`, requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        resultBody = result;
+        res.send({"result": resultBody.rates});
+      })
+      .catch(error => console.log('error', error));
+    
+    });
+  
+
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
