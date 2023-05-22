e766a2b5e7c8cc79727897ed4e2da8d25b08ed2f
Yao(Victor) Li <victorliyao@gmail.com>
2023-05-21 22:13:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -31,45 +31,85 @@ app.get('/con/:base/:target/:amo', (req, res) => {
     })
     .catch(error => console.log('error', error));
   
-	});
+});
 
-  app.get('/7day/:base/:target', (req, res) => {
-    console.log("welcome to the root!");
+
+app.get('/7day/:base/:target', (req, res) => {
+  console.log("welcome to the root!");
     
-    //Parameters
-    //base: default currency,  target: currency that convert to
-    var base = req.params.base;
-    var target = req.params.target;
+  //Parameters
+  //base: default currency,  target: currency that convert to
+  var base = req.params.base;
+  var target = req.params.target;
 
-    //Get Today's date
-    let date = new Date();
-    let today = date.toISOString().split("T")[0];
+  //Get Today's date
+  let date = new Date();
+  let today = date.toISOString().split("T")[0];
 
-    //Get the date of 7 days ago
-    date.setDate(date.getDate() - 7);
-    let day7 = date.toISOString().split("T")[0];
+  //Get the date of 7 days ago
+  date.setDate(date.getDate() - 7);
+  let day7 = date.toISOString().split("T")[0];
 
   
-    var myHeaders = new Headers();
-    myHeaders.append("apikey", "7OlEKeJ4zbqZGYvVYOrD4rMlLgWdCU5j");
+  var myHeaders = new Headers();
+  myHeaders.append("apikey", "7OlEKeJ4zbqZGYvVYOrD4rMlLgWdCU5j");
     
-    var requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-      headers: myHeaders
-    };
-    var resultBody;
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
+
+  var resultBody;
     
-    fetch(`https://api.apilayer.com/exchangerates_data/timeseries?start_date=${day7}&end_date=${today}&symbols=${target}&base=${base}`, requestOptions)
-      .then(response => response.json())
-      .then(result => {
-        resultBody = result;
-        res.send({"result": resultBody.rates});
-      })
-      .catch(error => console.log('error', error));
+  fetch(`https://api.apilayer.com/exchangerates_data/timeseries?start_date=${day7}&end_date=${today}&symbols=${target}&base=${base}`, requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      resultBody = result;
+      res.send({"result": resultBody.rates});
+    })
+    .catch(error => console.log('error', error));
     
-    });
+});
+
+app.get('/30day/:base/:target', (req, res) => {
+  console.log("welcome to the root!");
+      
+  //Parameters
+  //base: default currency,  target: currency that convert to
+  var base = req.params.base;
+  var target = req.params.target;
   
+  //Get Today's date
+  let date = new Date();
+  let today = date.toISOString().split("T")[0];
+  
+  //Get the date of 30 days ago
+  date.setDate(date.getDate() - 30);
+  let day30 = date.toISOString().split("T")[0];
+  
+  var myHeaders = new Headers();
+  myHeaders.append("apikey", "7OlEKeJ4zbqZGYvVYOrD4rMlLgWdCU5j");
+      
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
+  
+  var resultBody;
+      
+  fetch(`https://api.apilayer.com/exchangerates_data/timeseries?start_date=${day30}&end_date=${today}&symbols=${target}&base=${base}`, requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      resultBody = result;
+      res.send({"result": resultBody.rates});
+    })
+    .catch(error => console.log('error', error));
+      
+});
+  
+
 
 
 app.listen(3000, () => {
